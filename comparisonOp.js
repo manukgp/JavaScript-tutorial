bdf8f65e07af874cdd345f8c958f95fdd6c99ed2
manukgp <111724756+manukgp@users.noreply.github.com>
2024-12-19 14:32:38 +0000

    comparison op changes

--- comparisonOp.js
+++ comparisonOp.js
@@ -1,10 +1,10 @@
 /* In JS comparison between same data types is like in other languages, 
-    but when data types are different there type coercion starts.
+    but when data types are different, type coercion starts by default.
 
-    Equality check (==) and comparison operators (>, <, >=, <=) work differently for unequal data types.
+    * Equality check (==) and comparison operators (>, <, >=, <=) work differently for unequal data types.
     Comparison convert null (but not undefined) to 0 or NaN (a number) :
-    null >0 becomes 0 >0 -> false
-    But, null == 0 -> false ,as null not converted to 0
+
+    * Strict check: === -> checks value as well as data type without type coercion
  */
 
 // console.log(null == 0);
@@ -12,11 +12,8 @@
 // console.log(null >= 0);        // 0>=0 true
 
 /* In JS exp is evaluated from left to right 
-    and being loosely typed, it keeps converting while proceeding, wherever possible */
+    and being loosely typed, it keeps converting while proceeding, wherever possible.
+*/
 
 // console.log(1<2<3)          // left to right -> true<3 -> 1<3 -> true
 // console.log(3>2>1)          // left to right -> true>1 -> 1>1 -> false
-
-/* Strict check: ===
-    value and data type without type coercion
-*/
