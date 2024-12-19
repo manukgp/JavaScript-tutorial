5bbd55631a7c7b1662538f5564507ca985f1e9dd
manukgp <111724756+manukgp@users.noreply.github.com>
2024-12-19 11:21:04 +0000

    const and variables

---
+++ variables.js
@@ -0,0 +1,18 @@
+// for constants only
+const id = 1
+
+// ways to define variables
+let username = "User"
+var city = "Gaya"
+state = "Bihar"
+
+// variables can be initialised without a value :- undefined for now
+let Email;
+
+username = "user01"
+city = "Patna"
+state = "BR"
+
+// Don't use var as it has issues of block scope and functional scope. 
+
+console.table([id, username, city, state, Email]);
