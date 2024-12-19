9af9dac6593c818c87bdd3ea1e204f21895c9609
manukgp <111724756+manukgp@users.noreply.github.com>
2024-12-19 14:03:28 +0000

    type coercion

--- typeCoercion.js
+++ typeCoercion.js
@@ -4,7 +4,7 @@ value = Number(value)
 let data = "12mk"
 data = Number(data)
 
-console.log(value, data) // number, NaN
+// console.log(value, data) // number, NaN
 
 /* when converting to Number from:
 
@@ -20,3 +20,39 @@ console.log(value, data) // number, NaN
     null -> false
     undefined -> false
 */
+
+// ***********************Operations************************
+// Arithmetic operations
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+/* Operations specific to JS
+    in JS exp is evaluated from left to right 
+    and being loosely typed, it keeps converting while proceeding, wherever possible
+
+    when '+' is there, string/boolean is converted to number : +true -> 1, +"12" -> 12
+    Also, +12 => "12"
+*/
+
+// console.log("3"+2)          // 32
+// console.log("3"-2)          // 1
+// console.log("3"+ 2 + 5)     // left to right -> 325
+// console.log(3 + 2 + "5")    // left to right -> 5+"5" -> 55
+// console.log(1<2<3)          // left to right -> true<3 -> 1<3 -> true
+// console.log(3>2>1)          // left to right -> true>1 -> 1>1 -> false
+
+// console.log(8+4*2/4);       // operator precedence
+// console.log(+true);         // converted to string -> 1
+// console.log(+"");           // 0
+
+// increment operator
+
+// let count = 10
+// ++count
+// count++
+// console.log(count);         //12
