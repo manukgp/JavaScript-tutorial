7aaecca669eb0ac6b1655728b43ca7c034993623
manukgp <111724756+manukgp@users.noreply.github.com>
2024-12-19 12:51:04 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+// to declare that all the code be treated as per the newer guidelines (optional).
+"use strict";
+
+/* Primitive data types:
+number: 3, 0 , 3.14, NaN, Infinity, -Infinity
+BigInt
+string: "hello"
+boolean
+null: intentional absence of value
+undefined: initialised but not given value
+symbol: for unique immutable identifier
+*/
+
+let value;              //undefined
+let emptyValue = null;  //null
+
+// Note: typeof null -> Object
+//       null == undefined -> both represent empty value but types are different
+
+// Non primitive/ Reference type: Objects, Array, Function, Map, Set, Date
